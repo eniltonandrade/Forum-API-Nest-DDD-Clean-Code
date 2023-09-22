cf055274313eba3ce9f3c2c8857708eec49055ce
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-09-22 14:30:22 -0500

    Implemented Repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaQuestionsCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionsCommentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionsCommentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+  ],
 })
 export class DatabaseModule {}
