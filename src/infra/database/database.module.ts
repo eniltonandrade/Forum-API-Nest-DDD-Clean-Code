a906d18cf59d540ad1c2dd5732fcbf54470673cc
Enilton Rodrigues de Andrade <erodri@Eniltons-MacBook-Pro-2.local>
2023-09-26 16:54:43 -0500

    implements get question by slug controller and tests

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
-import { PrismaQuestionsCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
@@ -22,7 +22,7 @@ import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-
       useClass: PrismaStudentsRepository,
     },
     PrismaQuestionAttachmentsRepository,
-    PrismaQuestionsCommentsRepository,
+    PrismaQuestionCommentsRepository,
     PrismaAnswersRepository,
     PrismaAnswerAttachmentsRepository,
     PrismaAnswerCommentsRepository,
@@ -32,7 +32,7 @@ import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-
     QuestionsRepository,
     StudentsRepository,
     PrismaQuestionAttachmentsRepository,
-    PrismaQuestionsCommentsRepository,
+    PrismaQuestionCommentsRepository,
     PrismaAnswersRepository,
     PrismaAnswerAttachmentsRepository,
     PrismaAnswerCommentsRepository,
